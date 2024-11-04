9bbcdee41075e39eaacb2ccc70bae93adf1dc407
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2024-11-04 11:27:13 +0900

    [Silver IV] Title: 상근이의 여행, Time: 3896 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -0,0 +1,14 @@
+# 테스트케이스 수
+t = int(input())
+
+for _ in range(t):
+
+    # 국가, 비행기 정보
+    n, m = map(int, input().split())
+
+    for _ in range(m):
+        # 비행기 
+        a, b = map(int, input().split())
+
+    # 모든 국가 방문하기 위한 최소 비행기 종류
+    print(n-1)
