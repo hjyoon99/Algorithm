7b356a754f33cfa6dbce7c242987030276d49cb0
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2024-11-21 11:29:56 +0900

    [Silver IV] Title: 로프, Time: 2632 ms, Memory: 36148 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.py
+++ 백준/Silver/2217. 로프/로프.py
@@ -1,8 +1,3 @@
-# n 개의 로프
-# k 개의 로프 사용해 중량 w인 물체 ->  *고르게* w/k 만큼의 중량
-# 들어올릴 수 있는 최대 중량
-# 임의로 로프 골라도 됨
-
 n = int(input())
 max_ns = []
 
@@ -11,12 +6,13 @@ for i in range(n):
     max_ns.append(max_n)
 
 sorted_max_ns = sorted(max_ns)
-weights = []
+max_weight = 0
 tmp = n
 
 for j in range(n):
     weight = sorted_max_ns[j] * tmp
-    weights.append(weight)
+    if weight > max_weight:
+        max_weight = weight
     tmp -= 1
 
-print(max(weights))
+print(max_weight)
