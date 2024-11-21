a1db0bcf88119350f8fabfdae3358a60d4b7d1d2
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2024-11-21 11:32:42 +0900

    [Silver IV] Title: 로프, Time: 2588 ms, Memory: 35364 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.py
+++ 백준/Silver/2217. 로프/로프.py
@@ -1,18 +1,7 @@
 n = int(input())
-max_ns = []
+max_ns = [int(input()) for _ in range(n)]
 
-for i in range(n):
-    max_n = int(input())
-    max_ns.append(max_n)
-
-sorted_max_ns = sorted(max_ns)
-max_weight = 0
-tmp = n
-
-for j in range(n):
-    weight = sorted_max_ns[j] * tmp
-    if weight > max_weight:
-        max_weight = weight
-    tmp -= 1
+max_ns.sort()
+max_weight = max(max_ns[i] * (n-i) for i in range(n))
 
 print(max_weight)
