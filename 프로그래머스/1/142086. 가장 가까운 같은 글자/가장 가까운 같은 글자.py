bbc815c5141be4022c878a880ff8ab1fa28a6724
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2025-01-08 11:09:37 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 1.66 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,17 +1,13 @@
 def solution(s):
     prev_idx = {}
-    tmp = []
     result = []
     
     for i, value in enumerate(s):
-        if value not in tmp:
-            tmp.append(value)
+        if value not in prev_idx:
             result.append(-1)
-            prev_idx[value] = i
         else:
-            tmp.append(value)
             result.append(i-prev_idx[value])
-            prev_idx[value] = i
+        prev_idx[value] = i
         
         
     return result
