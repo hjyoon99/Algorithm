c41050d058aed90b0c75477c2e1f520639d0e913
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2025-01-17 11:37:01 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.03 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -0,0 +1,11 @@
+def solution(food):
+    answer = ''
+    for i in range(1, len(food)):
+        if(food[i]//2 >= 1):
+            answer += str(i) * (food[i]//2)
+
+    tmp = answer[::-1]
+    answer += '0'
+    answer += tmp
+    
+    return answer
