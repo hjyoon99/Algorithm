4cb9a8b4f7ff0bceb6ddff52e16309f2eeccda3d
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2025-06-11 14:12:45 +0900

    [level 0] Title: OX퀴즈, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
@@ -1,7 +1,7 @@
 function solution(quiz) {
     return quiz.map(q=>{
-        const [left, right] = q.split(" = ");
-        const result = eval(left);
-        return result === Number(right) ? "O" : "X";
+        const [x, op, y, , z] = q.split(" ");
+        const result = op === '+' ? Number(x) + Number(y) : Number(x) - Number(y);
+        return result === Number(z) ? "O" : "X";
     });
 }
