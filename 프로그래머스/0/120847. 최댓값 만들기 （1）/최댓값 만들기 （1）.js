1e4526920ead7ea82f8c375ea2e027dee2c729c7
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2025-06-12 12:38:03 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,7 +1,4 @@
 function solution(numbers) {
-    var max1 = Math.max(...numbers);
-    var index = numbers.indexOf(max1);
-    numbers.splice(index, 1);
-    var max2 = Math.max(...numbers);
-    return max1 * max2;
+    numbers.sort((a, b) => b - a);
+    return numbers[0] * numbers[1];
 }
