608bb3419c79350056f54a11f9ec1ba7562f5ac3
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2025-06-12 12:51:37 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,10 +1,4 @@
 function solution(array, height) {
-    array.sort((a, b) => b-a);
-    var sum = 0;
-    for (let i = 0; i < array.length; i++){
-        if(array[i] > height){
-            sum += 1
-        }else break;
-    }
-    return sum;
+    var answer = array.filter(arr => arr > height);
+    return answer.length;
 }
