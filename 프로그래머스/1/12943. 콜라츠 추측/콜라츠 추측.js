d2e11660de9c0ca50356897de3f555f8decb20a4
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2025-06-13 00:23:53 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,13 +1,12 @@
 function solution(num) {
     var i = 1;
-    var n = num;
     var cnt = 0;
     while (i <= 500){
         if (num === 1) return 0;
-        if (n % 2 === 0) {
-            n /= 2;
-        }else n = (n * 3) + 1;
-        if (n === 1) return i;
+        if (num % 2 === 0) {
+            num /= 2;
+        }else num = (num * 3) + 1;
+        if (num === 1) return i;
         i++;
     }
     return -1;
