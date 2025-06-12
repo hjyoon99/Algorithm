d4120cf0a42ecb4e71dadea96f5efaf74bf2fb8e
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2025-06-12 13:08:37 +0900

    [level 0] Title: 배열의 유사도, Time: 0.05 ms, Memory: 31.8 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,9 +1,4 @@
 function solution(s1, s2) {
-    var answer = 0;
-    for (const c of s1){
-        if (s2.includes(c)){
-            answer += 1;
-        }
-    }
-    return answer;
+    var answer = s1.filter(x => s2.includes(x));
+    return answer.length;
 }
