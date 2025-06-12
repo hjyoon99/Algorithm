81fe510fb12537ee8c84c8aaa521b77dd3cb3438
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2025-06-12 15:11:50 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -1,7 +1,3 @@
 function solution(my_string, n) {
-    var str = my_string.split("");
-    for(let i = 0; i < str.length; i++){
-        str.splice(i, 1, str[i].repeat(n));
-    }
-    return str.join("");
+    return [...my_string].map(x => x.repeat(n)).join("");
 }
