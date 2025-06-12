ea7b983be9ee49ea86e43f63421f476e5d955105
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2025-06-12 15:31:40 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.js
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.js
@@ -1,7 +1,3 @@
 function solution(num_list) {
-    if (num_list.length >= 11){
-        return num_list.reduce((sum, curr) => sum + curr);
-    }else{
-        return num_list.reduce((sum, curr) => sum * curr);
-    }
+    return num_list.length >= 11 ? num_list.reduce((sum, curr) => sum + curr) : num_list.reduce((sum, curr) => sum * curr);
 }
