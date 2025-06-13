15026b793dfcb9bfb0dad9bafc13ec2a4d3feff4
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2025-06-13 23:31:23 +0900

    [level 1] Title: 예산, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,12 +1,5 @@
 function solution(d, budget) {
-    d.sort((a, b) => a - b);
-    var count = 0;
-    var ok = 0;
-    for(let money of d){
-        ok += money;
-        if(ok <= budget){
-            count += 1;
-        }else break;
-    }
-    return count;
+    return d.sort((a, b) => a - b).reduce((count, money) => {
+        return count + ((budget -= money) >= 0);
+    }, 0)
 }
