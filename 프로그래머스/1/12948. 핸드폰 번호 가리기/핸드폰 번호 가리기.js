0dec3dae5eb7e9b6015107c543bb7494d827bcdc
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2025-06-13 19:04:27 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,9 +1,3 @@
 function solution(phone_number) {
-    var answer = ""
-    var num = phone_number.slice(-4);
-    for(let i = 0; i < phone_number.length - 4; i++){
-        answer += '*';
-    }
-    answer += num;
-    return answer;
+    return "*".repeat(phone_number.length -4) + phone_number.slice(-4);
 }
