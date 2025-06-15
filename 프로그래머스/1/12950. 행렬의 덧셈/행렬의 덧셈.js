3de2d7ab395b075edc3f11eafa5f3ad00c1de48d
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2025-06-15 16:35:52 +0900

    [level 1] Title: 행렬의 덧셈, Time: 13.05 ms, Memory: 67.7 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,11 +1,5 @@
 function solution(arr1, arr2) {
-    var answer = [];
-    for(let j = 0; j < arr1.length; j++){
-        var row = [];
-        for(let i = 0; i < arr1[j].length; i++){
-           row.push(arr1[j][i] + arr2[j][i]);
-        }
-        answer.push(row);
-    }
-    return answer;
+    return arr1.map((val, idx) => 
+                    val.map((val2, idx2) => 
+                    val2 + arr2[idx][idx2]));
 }
