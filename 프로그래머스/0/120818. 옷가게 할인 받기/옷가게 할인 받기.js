320465adc20d01c2f316903c37a3ae6354a2cb34
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2025-06-16 14:09:19 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,6 +1,6 @@
 function solution(price) {
-    if (price >= 500000) return Math.floor(price * 0.8);
-    if (price >= 300000) return Math.floor(price * 0.9); 
-    if (price >= 100000) return Math.floor(price * 0.95);
+    if (price >= 500000) return parseInt(price * 0.8);
+    if (price >= 300000) return parseInt(price * 0.9); 
+    if (price >= 100000) return parseInt(price * 0.95);
     else return price;
 }
