4651bf59230461d74c02f110aa7718baa455c2af
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2025-06-16 11:33:29 +0900

    [level 1] Title: 기사단원의 무기, Time: 142.91 ms, Memory: 38.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -0,0 +1,15 @@
+function solution(number, limit, power) {
+    var answer = [];
+    for(let j = 1; j <= number; j++){
+        var pow = 0;
+        for(i = 1; i * i <= j; i++){
+            if (j % i === 0) {
+                pow += (i*i === j) ? 1 : 2;
+            }
+        }
+        if (pow > limit) pow = power;
+        answer.push(pow);
+    }
+    
+    return answer.reduce((sum, curr) => sum + curr);
+}
