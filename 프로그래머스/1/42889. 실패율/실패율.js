31858d9ee4a59c83beb9541bb429ab1b3cc6a1d8
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2025-06-23 08:26:58 +0900

    [level 1] Title: 실패율, Time: 7.21 ms, Memory: 43.5 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,24 +1,19 @@
 function solution(N, stages) {
-    let answer = [];
-    let fails = [];
-    stages.sort((a, b) => b - a);
-    //console.log(stages);
+    var answer = [];
+    let stageCount = Array(N+2).fill(0);
+    
+    for(let s of stages){
+        stageCount[s] += 1;
+    }
+    
+    let totalPlayer = stages.length;
     
     for(let i = 1; i <= N; i++){
-        let clear = 0;
-        let no_clear = 0;
-        let fail = 0;
-        for(let person of stages){
-            // stage 도달 : i 보다 큰 값
-            if(person > i) clear += 1;
-            // 도달했으나 클리어 못한 : i 
-            if(person === i) no_clear += 1;
-        }
-        fail = no_clear / (clear + no_clear);
-        fails.push([i, fail])
+        let fail = stageCount[i] / totalPlayer;
+        answer.push([i, fail]);
+        totalPlayer -= stageCount[i];
     }
-    fails.sort((a, b) => b[1] - a[1]);
-    console.log(fails);
-    return fails.map((x) => x[0]);
+    answer.sort((a, b) => b[1] - a[1]);
     
+    return answer.map(a => a[0]);
 }
