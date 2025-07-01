e1b6a4f87d76ac100d3ccef77b17070c56ea0afd
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2025-07-01 09:54:33 +0900

    [level 2] Title: 전화번호 목록, Time: 155.04 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,9 +1,8 @@
 function solution(phone_book) {
     phone_book.sort();
-    
     for(let i = 0; i < phone_book.length-1; i++){
-        if (phone_book[i+1].startsWith(phone_book[i])){
-            return false;
+        if(phone_book[i+1].indexOf(phone_book[i]) === 0){
+            return false
         }
     }
     return true;
