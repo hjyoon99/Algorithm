62b1bc9c15b59932bb42c390dbfe15e61405cf4e
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2026-02-23 13:10:58 +0900

    [level 2] Title: H-Index, Time: 38.40 ms, Memory: 9.08 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -0,0 +1,16 @@
+def solution(citations):
+    answer = 0
+    citations.sort(reverse=True)
+    count = 0
+    for i in range(len(citations)):
+        h = i + 1
+        for cite in citations:
+            
+            if cite >= h:
+                count += 1
+        if count >= h:
+            answer = h
+        count = 0
+        
+            
+    return answer
