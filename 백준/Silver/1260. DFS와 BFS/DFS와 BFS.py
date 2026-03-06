32d52827dfed3cebda2b6e1b14a8aed0e6a2b51a
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2026-03-06 13:32:00 +0900

    [Silver II] Title: DFS와 BFS, Time: 328 ms, Memory: 35704 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,53 +1,44 @@
+# dfs 와 bfs
 from collections import deque
 
-# 입력
-n, m, v = map(int, input().split())
+def dfs(graph, visited, v):
+    visited[v] = True
+    print(v, end=' ')
 
-# 그래프 초기화
-graph = {i : [] for i in range(1, n+1)}
+    for next in graph[v]:
+        if not visited[next]:
+            dfs(graph, visited, next)
 
-# 간선 정보 입력
-for _ in range(m):
+def bfs(graph, visited, start):
+    q = deque([start])
+    visited[start] = True
+    while q:
+        v = q.popleft()
+        print(v, end=' ')
+
+        for next in graph[v]:
+            if not visited[next]:
+                visited[next] = True
+                q.append(next)
+
+
+n, m, v = list(map(int, input().split()))
+graph = [[] for _ in range(n+1)]
+visited = [False] * (n+1)
+
+for i in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
-# 인접 리스트 정렬 (정점 번호가 작은 순서부터 방문 위함)
-for nodes in graph.values():
-    nodes.sort()
+# 정점 번호가 작은 것부터 방문
+for i in range(1, n+1):
+    graph[i].sort()
 
-# dfs
-def dfs(graph, start_node):
-    # 방문할 노드 저장하는 deque
-    need_visited = deque([start_node])
-    # 방문한 노드 저장
-    visited = []
+dfs(graph, visited, v)
 
-    while need_visited:
-        node = need_visited.pop()
+print()
 
-        if node not in visited:
-            visited.append(node)
+visited = [False]*(n+1)
 
-            # 현재 노드의 인접노드 삽입
-            # 오른쪽에서 꺼내므로 역순으로 저장
-            need_visited.extend(reversed(graph[node]))
-        
-    return visited
-
-# bfs
-def bfs(graph, start_node):
-    need_visited = deque([start_node])
-    visited = []
-
-    while need_visited:
-        node = need_visited.popleft()
-        if node not in visited:
-            visited.append(node)
-            need_visited.extend(graph[node])
-
-    return visited
-
-# DFS와 BFS 결과 출력
-print(" ".join(map(str, dfs(graph, v))))
-print(" ".join(map(str, bfs(graph, v))))
+bfs(graph, visited, v)
