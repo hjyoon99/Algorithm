d38521fb3c6ea0defbc307de8eb20409bc57277a
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2026-03-06 21:45:12 +0900

    [Silver I] Title: 단지번호붙이기, Time: 32 ms, Memory: 32488 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,38 +1,31 @@
-# 2667 단지 번호 붙이기
+# 단지 번호 붙이기
+
+n = int(input())
+
+apt = [list(map(int, input())) for _ in range(n)]
 
 def dfs(x, y):
-    if x <= -1 or x >= n or y <= -1 or y >= n:
-        return False
+    if x < 0 or x >= n or y < 0 or y >= n:
+        return 0
     
-    if graph[x][y] == 1:
+    if apt[x][y] == 1:
+        apt[x][y] = 0
         count = 1
-        graph[x][y] = 0
         count += dfs(x-1, y)
         count += dfs(x+1, y)
         count += dfs(x, y-1)
         count += dfs(x, y+1)
+    
         return count
-    return False
+    return 0
 
-n = int(input())
-graph = []
-
-for i in range(n):
-    graph.append(list(map(int, input())))
-
-result = 0
-count = 0
-house = []
-
+result = []
 for i in range(n):
     for j in range(n):
-        home = dfs(i, j)
-        if home > 0:
-            result += 1
-            house.append(home)
+        if apt[i][j] == 1:
+            result.append(dfs(i, j))
 
-house.sort()
-
-print(result)
-for h in house:
-    print(h)
+print(len(result))
+result.sort()
+for i in result:
+    print(i)
